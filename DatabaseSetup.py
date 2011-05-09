85c136ff114f6056c5d8c98ded02f3d2f0d43209
ninj0x <ninj0x@gmail.com>
2011-05-08 21:59:36 -0700

    Initial commit for sctipt to set up a new database.

---
+++ DatabaseSetup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# This script will make a new sqlite3 database
+# for the SingularPassword project.
+# It also seeds the DB with the alphabet, upper and lower case.
+
+from sqlite3 import connect
+import hashlib
+import string 
+
+# Create a new DB if one is not there.
+conn = connect('singular.db')
+c = conn.cursor()
+c.execute('''create table passwords
+(hash text unique, count integer, isdictionary integer)''')
+conn.commit()
+
+#Seed the db with the alphabet.
+for letter in string.letters:
+	letterhash = hashlib.sha1(letter).hexdigest()
+	row = (letterhash, 1, 0)
+	c.execute('insert into passwords values (?,?,?)', row)
+
+conn.commit()
+
+c.close()
